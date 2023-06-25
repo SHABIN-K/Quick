b53e3b080d4e2d655fe725c1201509f274ac9467
Shabin-k <helloshabink@gmail.com>
2023-06-25 18:25:49 +0530

    feat: create container

---
+++ components/navbar/Logo.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { logo } from "@/public/images";
+
+const Logo = () => {
+  const router = useRouter();
+
+  return (
+    <Image
+      onClick={() => router.push("/")}
+      className="md:block cursor-pointer"
+      src={logo}
+      height="100"
+      width="100"
+      alt="Logo"
+    />
+  );
+};
+
+export default Logo;
---
+++ components/navbar/Search.tsx
@@ -0,0 +1,8 @@
+
+const Search = () => {
+  return (
+    <div>Search</div>
+  )
+}
+
+export default Search;
