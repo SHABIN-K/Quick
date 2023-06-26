fb5c470028402d436abbb74b7417818975a2f66a
Shabin-k <helloshabink@gmail.com>
2023-06-26 19:37:13 +0530

    htid

--- components/navbar/UserMenu.tsx
+++ components/navbar/UserMenu.tsx
@@ -24,7 +24,7 @@ const UserMenu = () => {
           className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
         >
           <AiOutlineMenu />
-          <div className="hidden md:block">
+          <div className="hidden md:block"> 
             <Avatar src={undefined} />
           </div>
         </div>
@@ -33,8 +33,8 @@ const UserMenu = () => {
         <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
           <div className="flex flex-col cursor-pointer">
             <>
-              <MenuItem label="Login" onClick={() => {}} />
-              <MenuItem label="Sign up" onClick={() => {}} />
+              <MenuItem label="Login" onClick={() => router.push("/")} />
+              <MenuItem label="Sign up" onClick={() => router.push("/")} />
             </>
           </div>
         </div>
