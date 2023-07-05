ef7be63267b38957f64860b12654373f0a727a7e
Shabin-k <helloshabink@gmail.com>
2023-07-05 12:45:23 +0530

    feat : fix registerModal

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -1,8 +1,67 @@
+"use client";
+
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+//import { signIn } from "next-auth/react";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { toast } from "react-hot-toast";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+import Modal from "./Modal";
+//import Input from "../inputs/Input";
+//import Heading from "../Heading";
+//import Button from "../Button";
+
+import useRegisterModal from "@/hooks/useRegisterModal";
 
 const RegisterModal = () => {
-  return (
-    <div>RegisterModal</div>
-  )
-}
+  const registerModal = useRegisterModal();
+  // const loginModal = useLoginModal();
+  const [isLoading, setIsLoading] = useState(false);
 
-export default RegisterModal
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    axios
+      .post("/api/register", data)
+      .then(() => {
+        toast.success("Registered!");
+        registerModal.onClose();
+        //loginModal.onOpen();
+      })
+      .catch((error) => {
+        toast.error(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <div>
+      <Modal
+        disabled={isLoading}
+        isOpen={registerModal.isOpen}
+        title="Register"
+        actionLabel="Continue"
+        onClose={registerModal.onClose}
+        onSubmit={handleSubmit(onSubmit)}
+      />
+    </div>
+  );
+};
+
+export default RegisterModal;
--- hooks/useRegisterModal.ts
+++ hooks/useRegisterModal.ts
@@ -7,7 +7,7 @@ interface RegisterModalStore {
 }
 
 const useRegisterModal = create<RegisterModalStore>((set) => ({
-  isOpen: false,
+  isOpen: true,
   onOpen: () => set({ isOpen: true }),
   onClose: () => set({ isOpen: false }),
 }));
