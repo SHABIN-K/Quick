48b2fde1d05042e7c78de24e50cf82ee48e3b8c4
Shabin-k <helloshabink@gmail.com>
2024-04-20 16:10:43 +0530

    feat : add prisma

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,30 @@
 // Import the 'express' module
 import express from 'express';
+import db from './libs/prismaDb';
 
 // Create an Express application
 const app = express();
 
 // Set the port number for the server
-const port = 3000;
+const port = 5000;
 
 // Define a route for the root path ('/')
 app.get('/', (req, res) => {
   // Send a response to the client
   res.send('Hello, TypeScript + Node.js + Express!');
 });
+
+async function createUser(email: string, name?: string) {
+  return await db.user.create({
+    data: {
+      email,
+      name,
+    },
+  });
+}
+
+// Usage
+createUser('example@email.com', 'John Doe');
 
 // Start the server and listen on the specified port
 app.listen(port, () => {
