95b5cd10e3f294183e15a5a1fe1cb67aef39dc47
Shabin-k <helloshabink@gmail.com>
2024-04-28 23:08:27 +0530

    feat : add image hostnames

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "avatar.iran.liara.run",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
