0bb753619d0176c05db5030b0dc0f60c6ef1a8c1
Shabin-k <helloshabink@gmail.com>
2024-05-05 15:21:46 +0530

    feat : add ui body

--- client/src/app/chats/[chatId]/components/Body.tsx
+++ client/src/app/chats/[chatId]/components/Body.tsx
@@ -1,15 +1,37 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
+import useConversation from "@/hooks/useConversation";
 import { FullMessageType } from "@/shared/types";
+import MessageBox from "./MessageBox";
+import axios from "@/config/api";
 
 interface BodyProps {
   initialMessages: FullMessageType[];
 }
 
 const Body: React.FC<BodyProps> = ({ initialMessages }) => {
-  console.log(initialMessages);
+  const [messages, setMessages] = useState(initialMessages);
+  const bottomRef = useRef<HTMLDivElement>(null);
 
-  return <div>Body</div>;
+  const { conversationId } = useConversation();
+
+  useEffect(() => {
+    axios.post(`/api/conversations/${conversationId}/seen`);
+  }, [conversationId]);
+
+  return (
+    <div className="flex-1 overflow-y-auto">
+      {messages.map((message, i) => (
+        <MessageBox
+          isLast={i === messages.length - 1}
+          key={message.id}
+          data={message}
+        />
+      ))}
+      <div ref={bottomRef} className="pt-24" />
+    </div>
+  );
 };
 
 export default Body;
