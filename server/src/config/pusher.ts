7d2a093190fd9c2c1fec3b43cc3e565d3a58a440
Shabin-k <helloshabink@gmail.com>
2024-05-06 13:11:48 +0530

    feat : add pusher in server

---
+++ server/src/config/pusher.ts
@@ -0,0 +1,9 @@
+import Pusher from 'pusher';
+
+export const pusherServer = new Pusher({
+  appId: process.env.PUSHER_APP_ID!,
+  key: process.env.PUSHER_APP_KEY!,
+  secret: process.env.PUSHER_APP_SECRET!,
+  cluster: process.env.PUSHER_APP_CLUSTER as string,
+  useTLS: true,
+});
