eb0f527cca62cefd10bafed6e8fa3cbb5da019b0
Shabin-k <helloshabink@gmail.com>
2024-05-07 23:54:57 +0530

    updated

--- server/src/config/pusher.ts
+++ server/src/config/pusher.ts
@@ -1,9 +1,9 @@
 import Pusher from 'pusher';
 
 export const pusherServer = new Pusher({
-  appId: process.env.PUSHER_APP_ID!,
-  key: process.env.PUSHER_APP_KEY!,
-  secret: process.env.PUSHER_APP_SECRET!,
-  cluster: process.env.PUSHER_APP_CLUSTER as string,
+  appId: process.env.APP_PUSHER_ID!,
+  key: process.env.APP_PUSHER_KEY!,
+  secret: process.env.APP_PUSHER_SECRET!,
+  cluster: process.env.APP_PUSHER_CLUSTER as string,
   useTLS: true,
 });
