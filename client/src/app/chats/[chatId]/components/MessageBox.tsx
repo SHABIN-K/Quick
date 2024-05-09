805314188e448d89fdc93ab92ddf3e89ed9c83f1
Shabin-k <helloshabink@gmail.com>
2024-05-09 16:11:56 +0530

    refactor(client) : improve code readabilty

--- client/src/app/chats/[chatId]/components/MessageBox.tsx
+++ client/src/app/chats/[chatId]/components/MessageBox.tsx
@@ -16,36 +16,31 @@ const MessageBox: React.FC<MessageBoxProps> = ({ data, isLast }) => {
   const { getSession } = useSession();
 
   const isOwn = getSession?.email === data?.sender?.email;
+  
   const seenList = (data.seen || [])
     .filter((user) => user.email !== data?.sender?.email)
     .map((user) => user.name)
     .join(", ");
 
-  const container = clsx("flex gap-3 p-4", isOwn && "justify-end");
-
-  const avatar = clsx(isOwn && "order-2");
-
-  const body = clsx("flex flex-col gap-2", isOwn && "items-end");
-
-  const message = clsx(
-    "text-sm w-fit overflow-hidden",
-    isOwn ? "bg-sky-500 text-white" : "bg-gray-100",
-    data.image ? "rounded-md p-0" : "rounded-full py-2 px-3"
-  );
-
   return (
-    <div className={container}>
-      <div className={avatar}>
+    <div className={clsx("flex gap-3 p-4", isOwn && "justify-end")}>
+      <div className={clsx(isOwn && "order-2")}>
         <Avatar user={data.sender} />
       </div>
-      <div className={body}>
+      <div className={clsx("flex flex-col gap-2", isOwn && "items-end")}>
         <div className="flex items-center gap-1">
           <div className="text-sm text-gray-500">{data.sender.name}</div>
           <div className="text-xs text-gray-400">
             {format(new Date(data.createdAt), "p")}
           </div>
         </div>
-        <div className={message}>
+        <div
+          className={clsx(
+            "text-sm w-fit overflow-hidden",
+            isOwn ? "bg-sky-500 text-white" : "bg-gray-100",
+            data.image ? "rounded-md p-0" : "rounded-full py-2 px-3"
+          )}
+        >
           <div>{data.body}</div>
         </div>
         {isLast && isOwn && seenList.length > 0 && (
