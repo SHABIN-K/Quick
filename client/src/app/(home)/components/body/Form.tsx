42d0ad62adac8722769c9c34696115ff24df89ee
Shabin-k <helloshabink@gmail.com>
2024-05-30 01:53:27 +0530

    feat : add emoji picker

--- client/src/app/(home)/components/body/Form.tsx
+++ client/src/app/(home)/components/body/Form.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import React from "react";
+import { BsEmojiWink } from "react-icons/bs";
 import { HiPaperAirplane } from "react-icons/hi2";
+import EmojiPicker, { EmojiClickData } from "emoji-picker-react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import axios from "@/config/api";
@@ -9,11 +12,14 @@ import useConversation from "@/hooks/useConversation";
 
 const Form = () => {
   const { conversationId } = useConversation();
+  const [open, setOpen] = React.useState<boolean>(false);
+  const emojiPickerRef = React.useRef<HTMLDivElement>(null);
 
   const {
     register,
     handleSubmit,
     setValue,
+    getValues,
     formState: { errors },
   } = useForm<FieldValues>({
     defaultValues: {
@@ -30,12 +36,46 @@ const Form = () => {
     });
   };
 
+  const handleEmoji = (emojiObject: EmojiClickData) => {
+    const { message } = getValues();
+    setValue("message", message + emojiObject.emoji);
+    setOpen(false);
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      emojiPickerRef.current &&
+      !emojiPickerRef.current.contains(event.target as Node)
+    ) {
+      setOpen(false);
+    }
+  };
+
+  React.useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="py-4 px-4 bg-white border-t flex items-center gap-2 lg:gap-4 w-full">
       <form
         onSubmit={handleSubmit(onSubmit)}
         className="flex items-center gap-2 lg:gap-4 w-full"
       >
+        <div ref={emojiPickerRef}>
+          <p
+            onClick={() => setOpen(true)}
+            className="relative rounded-full p-2 cursor-pointer transition hover:bg-neutral-100"
+          >
+            <BsEmojiWink size={20} className="text-sky-400" />
+            <div className="absolute bottom-[60px]">
+              <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+            </div>
+          </p>
+        </div>
+
         <MessageInput
           id="message"
           register={register}
