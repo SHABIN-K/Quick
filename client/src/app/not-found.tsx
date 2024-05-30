4d3dd4a66326e3fe302b6b6c395a7e4d26bfef0a
Shabin-k <helloshabink@gmail.com>
2024-05-30 18:32:33 +0530

    feat : add dropdown in mobile device and improve style in not found js

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -6,9 +6,9 @@ import Image from "next/image";
 const NotFound = () => {
   return (
     <div className="h-screen w-screen bg-gray-50 flex items-center">
-      <div className="container flex flex-col md:flex-row items-center justify-between px-5 text-gray-700">
+      <div className="container flex flex-col md:flex-row items-center justify-between px-5 text-gray-500">
         <div className="w-full lg:w-1/2 mx-8">
-          <div className="text-7xl text-green-500 font-dark font-extrabold mb-8">
+          <div className="text-7xl text-sky-500 font-dark font-extrabold mb-8">
             404
           </div>
           <p className="text-2xl md:text-3xl font-light leading-normal mb-8">
@@ -17,7 +17,7 @@ const NotFound = () => {
 
           <Link
             href="/chats"
-            className="px-5 inline py-3 text-sm font-medium leading-5 shadow-2xl text-white transition-all duration-400 border border-transparent rounded-lg focus:outline-none bg-green-600 active:bg-red-600 hover:bg-red-700"
+            className="px-5 inline py-3 text-sm font-medium leading-5 shadow-2xl text-white transition-all duration-400 border border-transparent rounded-lg focus:outline-none bg-sky-500 hover:bg-sky-600 focus-visible:outline-sky-600"
           >
             back to homepage
           </Link>
--- client/src/components/sidebar/MobileFooter.tsx
+++ client/src/components/sidebar/MobileFooter.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import Link from "next/link";
+import { Fragment } from "react";
+import { Menu } from "@headlessui/react";
+import { CgProfile } from "react-icons/cg";
+
+import Avatar from "../Avatar";
 import DisplayItem from "./DisplayItems";
+import useAuthStore from "@/store/useAuth";
 import useNavigation from "@/hooks/useNavigation";
 import useConversation from "@/hooks/useConversation";
 
 const MobileFooter = () => {
   const routes = useNavigation();
+  const { session } = useAuthStore();
   const { isOpen } = useConversation();
 
   if (isOpen) {
@@ -25,6 +33,50 @@ const MobileFooter = () => {
           mobileOnly={item.mobileOnly}
         />
       ))}
+      <Menu>
+        <Menu.Button>
+          <div className="relative cursor-pointer hover:opacity-75 transition ring-2 ring-sky-500 rounded-full mt-2">
+            {session && <Avatar user={session} />}
+          </div>
+        </Menu.Button>
+        <Menu.Items className="absolute right-2 bottom-[60px] w-48 divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+          <div className="px-1 py-1 ">
+            <Menu.Item>
+              {({ active }) => (
+                <Link
+                  href="/profile"
+                  className={`${
+                    active
+                      ? "bg-sky-50 text-sky-500"
+                      : "text-gray-500 hover:text-sky-500 hover:bg-sky-50"
+                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                >
+                  <CgProfile className="mr-2 h-5 w-5" />
+                  <span>Profile</span>
+                </Link>
+              )}
+            </Menu.Item>
+            {routes.slice(3, 5).map((item) => (
+              <Menu.Item key={item.label} as={Fragment}>
+                {({ active }) => (
+                  <Link
+                    href={item.href || ""}
+                    onClick={item.onClick}
+                    className={`${
+                      active
+                        ? "bg-sky-50 text-sky-500"
+                        : "text-gray-500 hover:text-sky-500 hover:bg-sky-50"
+                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                  >
+                    <item.icon className="mr-2 h-5 w-5" />
+                    <span>{item.label}</span>
+                  </Link>
+                )}
+              </Menu.Item>
+            ))}
+          </div>
+        </Menu.Items>
+      </Menu>
     </div>
   );
 };
